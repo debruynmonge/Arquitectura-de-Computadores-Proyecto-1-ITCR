cb00ad1ce89cc989c71633ce9a6402329765996a
debruynmonge <42415865+debruynmonge@users.noreply.github.com>
2018-08-15 13:07:05 -0600

    Add files via upload

---
+++ ejempos_basicos/display_digit.asm
@@ -0,0 +1,22 @@
+section .data
+      digit db 0, 10
+
+section .text
+         global _start
+
+_start:
+    call _printDigit
+        
+     mov rax, 60
+     mov rdi, 0
+     syscall
+
+_printDigit:
+    add rax, 48
+    mov [digit], al
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, digit
+    mov rdx, 2
+    syscall
+    ret
