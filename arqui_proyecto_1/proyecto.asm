5b39842b87ff82e7b1f838b33b8d147215ed9758
debruynmonge <42415865+debruynmonge@users.noreply.github.com>
2018-08-15 14:49:59 -0600

    Add files via upload

---
+++ arqui_proyecto_1/proyecto.asm
@@ -0,0 +1,15 @@
+;proyecto 1 arquitectura de Computadores
+
+section .data
+; aqui se definen las varibles inicializadas
+
+section .bss
+;aqui se definen las variables no inicializadas
+
+
+
+section .text
+	global _start
+	_start:    ; aqui comienza el programa
+
+
